905d9056d4a956606f6c382d51be249271bc2bf3
MisterCavespider <cavespider12@gmail.com>
2018-04-02 22:03:59 +0200

    Populated Model just a little bit

--- model_model.h
+++ model_model.h
@@ -8,10 +8,22 @@
 #ifndef MODEL_MODEL_H_
 #define MODEL_MODEL_H_
 
+#include <stdint.h>
+
+#include "model_module.h"
+
 class Model {
 public:
 	Model();
+	void bakeAudioFrom();
 	virtual ~Model();
+private:
+	uint8_t size;
+	uint8_t count;
+	Module **modules;
+
+	// Audio
+	AudioControlSGTL5000	sgtl5000;
 };
 
 #endif /* MODEL_MODEL_H_ */
